a694ac5e39cfbc4a0cf57ab647cc88d9461360bc
Ken Smith <kennethwsmith@gmail.com>
2020-04-16 10:50:14 -0500

    updating tests

--- tests/test_vehicle.py
+++ tests/test_vehicle.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 import re
-
-from src.vehicle import VehicleProvider  # noqa
+from faker_vehicle import VehicleProvider  # noqa
 
 def test_content_type(fake):
     assert True
 
 def test_server_token(fake):
-    assert True
+    assert False
 
 def test_server_token_solo(fake):
     assert True
