bea5c2ce6983a91feed583eebe68004ee9da3b78
Ken Smith <kennethwsmith@gmail.com>
2020-04-15 19:04:47 -0500

    changing web to vehicle

--- README.rst
+++ README.rst
@@ -5,11 +5,12 @@ faker_vehicle
 
 faker_vehicle is a provider for the `Faker`_ Python package.
 
-It provides web-related fake data for testing purposes:
+It provides vehicle related fake data for testing purposes:
 
-   * Content-type http header.
-   * Popular web server version tokens/signatures.
-
+    * Vehicle Makes
+    * Vehicle Models
+    * Vehicle Make / Model pairs
+    * Vehicle Years
 
 Usage
 -----
@@ -27,24 +28,33 @@ Or install with setup.py
     git clone https://github.com/kennethwsmith/faker_vehicle.git
     cd faker_vehicle && python setup.py install
 
-Add the ``WebProvider`` to your ``Faker`` instance:
+Add the ``VehicleProvider`` to your ``Faker`` instance:
 
 .. code:: python
 
     from faker import Faker
-    from faker_vehicle import WebProvider
+    from faker_vehicle import VehicleProvider
 
     fake = Faker()
-    fake.add_provider(WebProvider)
+    fake.add_provider(VehicleProvider)
 
-    fake.content_type()
-    # application/mxf
-    fake.content_type_popular()
-    # text/html
-    fake.server_token()
-    # Apache/2.0.51 (Ubuntu)
-
-
+    fake.vehicle_year_make_model()
+    # 2014 Volkswagen Golf
+    fake.vehicle_year_make_model_cat()
+    # 1996 Dodge Ram Wagon 1500 (Van/Minivan)
+    fake.vehicle_make_model()
+    # Volvo V40
+    fake.vehicle_make()
+    # BMW
+    fake.vehicle_model()
+    # SL
+    fake.vehicle_year()
+    # 2008
+    fake.vehicle_category()
+    # Wagon
+    json.dumps(fake.vehicle_object())
+    # {"Year": 1996, "Make": "Chrysler", "Model": "New Yorker", "Category": "Sedan"}
+    
 .. |pypi| image:: https://img.shields.io/pypi/v/faker_vehicle.svg?style=flat-square&label=version
     :target: https://pypi.python.org/pypi/faker_vehicle
     :alt: Latest version released on PyPi
--- src/vehicle/__init__.py
+++ src/vehicle/__init__.py
@@ -1,182 +1,64 @@
 # -*- coding: utf-8 -*-
 from faker.providers import BaseProvider
+from random import randint
+from .vehicle_dict import vehicles
 
-from .mimetypes import mime_types
-
-
-class WebProvider(BaseProvider):
+class VehicleProvider(BaseProvider):
     """
     A Provider for vehicle related test data.
 
     >>> from faker import Faker
-    >>> from faker_vehicle import WebProvider
+    >>> from faker_vehicle import VehicleProvider
     >>> fake = Faker()
-    >>> fake.add_provider(WebProvider)
+    >>> fake.add_provider(VehicleProvider)
     """
-    all_mime_types = mime_types
 
-    popular_makes = (
-        'Ford',
-        'Toyota',
-        'Volkswagen',
-        'Chevrolet'
-    )
+    def vehicle_object(self):
+        # Returns a random vehicle dict example: {"Year": 2008, "Make": "Jeep", "Model": "Wrangler", "Category": "SUV"}
+        randItem = randint(1,len(vehicles))
+        veh = vehicles[randItem]
+        return veh
 
-    popular_models = (
-        'G80',
-        'Mustang',
-        'Passat',
-        'F150',
-    )
+    def vehicle_year_make_model(self):
+        # Returns Year Make Model example: "1997 Nissan 240SX"
+        veh = self.vehicle_object()
+        year = veh.get('Year')
+        make = veh.get('Make')
+        model = veh.get('Model')
+        return str(year) + ' ' + make + ' ' + model
 
-    # popular_make_models = (
-    #     'Genesis G80',
-    #     'Ford Mustang',
-    #     'Volkswagen Passat',
-    #     'Ford F150',
-    # )
+    def vehicle_year_make_model_cat(self):
+        # Returns Year Make Model Cat example: 2017 GMC Sierra 1500 Double Cab (Pickup)
+        veh = self.vehicle_object()
+        year = veh.get('Year')
+        make = veh.get('Make')
+        model = veh.get('Model')
+        cat = veh.get('Category')
+        return str(year) + ' ' + make + ' ' + model + ' (' + cat + ')'
 
-    make_models = {
-        'Ford': [
-            'F150',
-            'Mustang'
-        ],
-        'Chevrolet': [
-            'Tahoe',
-            'Camaro'
-        ]
-    }
+    def vehicle_make_model(self):
+        # Returns Make Model example: Audi Q7
+        veh = self.vehicle_object()
+        make = veh.get('Make')
+        model = veh.get('Model')
+        return make + ' ' + model
 
-    def make_model(self):
-        """
-        Returns a vehicle make and model
+    def vehicle_make(self):
+        # Returns Make example: Lincoln
+        veh = self.vehicle_object()
+        return veh.get('Make')
 
-        >>> fake.make_model()
-        Chevrolet Camaro
+    def vehicle_year(self):
+        # Returns Year example: 1999
+        veh = self.vehicle_object()
+        return str(veh.get('Year'))
 
-        :return: make-model
-        :rtype: str
-        """
-        return self.random_element(self.make_models.keys())
+    def vehicle_model(self):
+        # Returns Model example: Frontier King Cab
+        veh = self.vehicle_object()
+        return veh.get('Model')
 
-
-    popular_mime_types = {
-        'application/javascript': ['js'],
-        'application/json': ['json'],
-        'application/pdf': ['pdf'],
-        'image/jpeg': ['jpeg', 'jpg', 'jpe'],
-        'image/gif': ['gif'],
-        'image/png': ['png'],
-        'image/svg+xml': ['svg', 'svgz'],
-        'text/css': ['css'],
-        'text/html': ['html', 'htm'],
-        'text/plain': ['txt', 'text', 'conf', 'def', 'list', 'log', 'in'],
-    }
-
-    web_servers = (
-        'apache',
-        'nginx',
-        'iis',
-        'varnish',
-    )
-
-    apache_versions = {
-        '1.3': list(range(1, 42)),
-        '2.0': list(range(35, 65)),
-        '2.2': list(range(0, 32)),
-        '2.4': list(range(1, 25)),
-    }
-
-    apache_distro = (
-        'Amazon',
-        'CentOS',
-        'Debian',
-        'Fedora',
-        'Red Hat',
-        'Ubuntu',
-        'Unix',
-    )
-
-    nginx_versions = {
-        '1.4': list(range(0, 7)),
-        '1.5': list(range(0, 13)),
-        '1.6': list(range(0, 3)),
-        '1.7': list(range(0, 10)),
-        '1.8': list(range(0, 1)),
-        '1.9': list(range(0, 15)),
-        '1.10': list(range(0, 3)),
-        '1.11': list(range(0, 8)),
-        '1.12': list(range(0, 0)),
-    }
-
-    iis_versions = (
-        '1.0',
-        '2.0',
-        '3.0',
-        '4.0',
-        '5.0',
-        '5.1',
-        '6.0',
-        '7.0',
-        '7.5',
-        '8.0',
-    )
-
-    def content_type(self):
-        """
-        Returns a mime-type from the list of types understood by the Apache
-        http server.
-
-        >>> fake.content_type()
-        application/mxf
-
-        :return: content-type
-        :rtype: str
-        """
-        return self.random_element(self.all_mime_types.keys())
-
-    def content_type_popular(self):
-        """
-        Returns a popular mime-type.
-
-        >>> fake.content_type_popular()
-        text/html
-
-        :return: content-type/mime-type
-        :rtype: str
-        """
-        return self.random_element(self.popular_mime_types.keys())
-
-    def _web_server_version(self, choices):
-        minor = self.random_element(choices.keys())
-        patch = self.random_element(choices.get(minor))
-        return '{minor}.{patch}'.format(**locals())
-
-    def apache(self):
-        version = self._web_server_version(self.apache_versions)
-        os = self.random_element(self.apache_distro)
-        return 'Apache/{version} ({os})'.format(**locals())
-
-    def nginx(self):
-        version = self._web_server_version(self.apache_versions)
-        return 'nginx/' + version
-
-    def iis(self):
-        return 'Microsoft-IIS/' + self.random_element(self.iis_versions)
-
-    def varnish(self):
-        return 'Varnish'
-
-    def server_token(self):
-        """
-        Returns a http web server response header, as per RFC-2616, section
-        14.38
-        https://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.38
-
-        >>> fake.server_token()
-        Apache/2.4.9 (Unix)
-
-        :return: web server signature.
-        :rtype: str
-        """
-        return getattr(self, self.random_element(self.web_servers))()
+    def vehicle_category(self):
+        # Returns Category example: SUV
+        veh = self.vehicle_object()
+        return veh.get('Category')
