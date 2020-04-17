49a536177c7e9277583d614844e8da474431cfc9
Ken Smith <kennethwsmith@gmail.com>
2020-04-17 11:00:43 -0500

    moved test setup to conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+import os
+import pytest
+import sys
+import json
+
+from faker import Faker
+
+local_path = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.join(local_path, '..'))
+from src.vehicle import VehicleProvider  # noqa
+
+@pytest.fixture(scope='module')
+def fake():
+    fixture = Faker()
+    fixture.add_provider(VehicleProvider)
+    return fixture
+
+@pytest.fixture
+def vehicles():
+    from src.vehicle.vehicle_dict import vehicles as vd
+    fixture = vd
+    return fixture
+
+@pytest.fixture
+def years():
+    from src.vehicle.vehicle_dict import vehicles as vd
+    year_dict = []
+    for veh in vd:
+        year_dict.append(int(veh['Year']))
+    year_dict = list(set(year_dict))
+    fixture = year_dict
+    return fixture
+
+@pytest.fixture
+def models():
+    from src.vehicle.vehicle_dict import vehicles as vd
+    model_dict = []
+    for veh in vd:
+        model_dict.append(veh['Model'])
+    model_dict = list(set(model_dict))
+    fixture = model_dict
+    return fixture
+
+@pytest.fixture
+def categories():
+    from src.vehicle.vehicle_dict import vehicles as vd
+    cat_dict = []
+    for veh in vd:
+        cat_dict.append(veh['Category'])
+    cat_dict = list(set(cat_dict))
+    fixture = cat_dict
+    return fixture
+
+@pytest.fixture
+def makes():
+    from src.vehicle.vehicle_dict import vehicles as vd
+    make_dict = []
+    for veh in vd:
+        make_dict.append(veh['Make'])
+    make_dict = list(set(make_dict))
+    fixture = make_dict
+    return fixture
--- tests/test_vehicle.py
+++ tests/test_vehicle.py
@@ -1,59 +1,37 @@
 # -*- coding: utf-8 -*-
 import re
 import pytest
-from faker import Faker
-from faker_vehicle import VehicleProvider  # noqa
-from src.vehicle.vehicle_dict import vehicles
 
-fake = Faker()
-fake.add_provider(VehicleProvider)
-
-def test_make():
-    make_list = []
-    for vehicle in vehicles:
-        make_list.append(vehicle['Make'])
-    make_list = list(set(make_list))
+def test_make(fake,makes):
     make = fake.vehicle_make()
     assert len(make) > 1
-    assert make in make_list
+    assert make in makes
 
-def test_year():
-    year_list = []
-    for vehicle in vehicles:
-        year_list.append(vehicle['Year'])
-    year_list = list(set(year_list))
+def test_year(fake,years):
     year = fake.vehicle_year()
     assert len(year) > 1
-    assert int(year) in year_list
+    assert int(year) in years
 
-def test_model():
-    model_list = []
-    for vehicle in vehicles:
-        model_list.append(vehicle['Model'])
-    model_list = list(set(model_list))
+def test_model(fake,models):
     model = fake.vehicle_model()
     assert len(model) > 1
-    assert model in model_list
+    assert model in models
 
-def test_category():
-    category_list = []
-    for vehicle in vehicles:
-        category_list.append(vehicle['Category'])
-    category_list = list(set(category_list))
+def test_category(fake,categories):
     category = fake.vehicle_category()
     assert len(category) > 1
-    assert category in category_list
+    assert category in categories
 
-def test_vehicle_make_model():
+def test_vehicle_make_model(fake):
     ar_mm = fake.vehicle_make_model().split()
     assert len(ar_mm) >= 1 # check to see if there are 2 words
 
-def test_vehicle_year_make_model():
+def test_vehicle_year_make_model(fake):
     ar_ymm = fake.vehicle_year_make_model().split()
     assert len(ar_ymm) >= 2 # check to see if there are 3 words
     assert float(ar_ymm[0]).is_integer() # check to see if first word is a number (year)
 
-def test_vehicle_year_make_model_cat():
+def test_vehicle_year_make_model_cat(fake):
     ar_ymmc = fake.vehicle_year_make_model_cat().split()
     assert len(ar_ymmc) >= 3 # check to see if there are 4 words
     assert float(ar_ymmc[0]).is_integer() # check to see if first word is a number (year)
