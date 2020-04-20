ced19a40dc0a64b2242675b1088b3e527acd18b5
Ken Smith <kennethwsmith@gmail.com>
2020-04-20 12:50:14 -0500

    0.1.3 cleanup

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 here = os.path.abspath(os.path.dirname(__file__))
 README = io.open(os.path.join(here, 'README.rst'), encoding="utf8").read()
 
-version = '0.1.2'
+version = '0.1.3'
 
 tests_require = [
     'pytest',
