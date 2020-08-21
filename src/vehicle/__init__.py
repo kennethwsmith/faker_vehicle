6c79882a5d0cd48cfb226c20562d278443dfe47e
qwanysh <kuanysh.beisembayev@gmail.com>
2020-08-21 17:45:33 +0600

    fix docstrings in VehicleProvider methods

--- src/vehicle/__init__.py
+++ src/vehicle/__init__.py
@@ -17,13 +17,16 @@ class VehicleProvider(BaseProvider):
     """
 
     def vehicle_object(self):
-        # Returns a random vehicle dict example: {"Year": 2008, "Make": "Jeep", "Model": "Wrangler", "Category": "SUV"}
-        randItem = randint(0,len(vehicles)-1)
+        """
+        Returns a random vehicle dict example:
+        {"Year": 2008, "Make": "Jeep", "Model": "Wrangler", "Category": "SUV"}
+        """
+        randItem = randint(0, len(vehicles) - 1)
         veh = vehicles[randItem]
         return veh
 
     def vehicle_year_make_model(self):
-        # Returns Year Make Model example: "1997 Nissan 240SX"
+        """Returns Year Make Model example: 1997 Nissan 240SX"""
         veh = self.vehicle_object()
         year = veh.get('Year')
         make = veh.get('Make')
@@ -31,7 +34,10 @@ class VehicleProvider(BaseProvider):
         return str(year) + ' ' + make + ' ' + model
 
     def vehicle_year_make_model_cat(self):
-        # Returns Year Make Model Cat example: 2017 GMC Sierra 1500 Double Cab (Pickup)
+        """
+        Returns Year Make Model Cat example:
+        2017 GMC Sierra 1500 Double Cab (Pickup)
+        """
         veh = self.vehicle_object()
         year = veh.get('Year')
         make = veh.get('Make')
@@ -40,28 +46,28 @@ class VehicleProvider(BaseProvider):
         return str(year) + ' ' + make + ' ' + model + ' (' + cat + ')'
 
     def vehicle_make_model(self):
-        # Returns Make Model example: Audi Q7
+        """Returns Make Model example: Audi Q7"""
         veh = self.vehicle_object()
         make = veh.get('Make')
         model = veh.get('Model')
         return make + ' ' + model
 
     def vehicle_make(self):
-        # Returns Make example: Lincoln
+        """Returns Make example: Lincoln"""
         veh = self.vehicle_object()
         return veh.get('Make')
 
     def vehicle_year(self):
-        # Returns Year example: 1999
+        """Returns Year example: 1999"""
         veh = self.vehicle_object()
         return str(veh.get('Year'))
 
     def vehicle_model(self):
-        # Returns Model example: Frontier King Cab
+        """Returns Model example: Frontier King Cab"""
         veh = self.vehicle_object()
         return veh.get('Model')
 
     def vehicle_category(self):
-        # Returns Category example: SUV
+        """Returns Category example: SUV"""
         veh = self.vehicle_object()
         return veh.get('Category')
